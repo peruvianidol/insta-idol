1cee9ce608b54fa6b2754eb2060802c735adf900
Mike Aparicio <peruvianidol@gmail.com>
2025-02-27 13:26:42 -0600

    Ensure _headers is copied to _site via Eleventy passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("_src/css");
   eleventyConfig.addPassthroughCopy("_src/js");
+  eleventyConfig.addPassthroughCopy("_headers");
 
   eleventyConfig.addFilter("limit", function (arr, limit) {
     return arr.slice(0, limit);
