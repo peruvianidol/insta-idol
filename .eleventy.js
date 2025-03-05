62ce12d03ee8e8ab370d4b7af7844ed405f8430e
Mike Aparicio <peruvianidol@gmail.com>
2025-03-05 09:59:06 -0600

    Add social image

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("_src/css");
   eleventyConfig.addPassthroughCopy("_src/js");
-
+  eleventyConfig.addPassthroughCopy("_src/images");
+  
   eleventyConfig.addFilter("limit", function (arr, limit) {
     return arr.slice(0, limit);
   });
